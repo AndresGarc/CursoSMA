175b9c07a9a4c343dc3797a342a8907e144f7a59
Andres Garcia <andres.garciamiras@gmail.com>
2020-10-08 01:22:11 +0200

    Get a raiz

--- backend/index.js
+++ backend/index.js
@@ -11,3 +11,15 @@ const app = express(); //creamos la app de express
 app.listen(3000, () => {
     console.log('Servidor corriendo en el puerto ' + 3000);
 });
+
+//PRIMER GET --> hace un get al recurs raiz, y responde con un mensaje
+//primer parametro -> ruta
+//segundo parametro ??
+app.get('/', (req, res) => {
+    //le enviamos una respuesta en forma de json????
+    //este mensaje es personalizable A PODER SER TODOS IGUALES
+    res.json({
+        ok: true,
+        msg: 'Respuesta'
+    });
+});
